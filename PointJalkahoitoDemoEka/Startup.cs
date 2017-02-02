8438788976981846c667b3ca4eedc9f489b893a0
Eero Kauppi <eero.kauppi@stud.pointcollege.fi>
2017-02-02 08:30:22 +0200

    Add project files.

---
+++ PointJalkahoitoDemoEka/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PointJalkahoitoDemoEka.Startup))]
+namespace PointJalkahoitoDemoEka
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
